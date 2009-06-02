0a86da1d34ecbdcda2979cc1aabe7c1c0fceff80
Paul V. Borza <paul@pvborza.com>
2009-06-02 11:23:04 +0300

    Initial version of Andrews algorithm for formal concepts.

---
+++ conexplore/winconexplore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using libconexplore;
+
+namespace winconexplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loader cLoader = new Loader();
+            //cLoader.Load("triangles.xml");
+            //cLoader.Load("numbers.xml");
+            cLoader.Load("mushrooms.xml");
+            //cLoader.Load("ads.xml");
+
+            //Naive cNaive = new Naive();
+            //cNaive.Compute(cLoader.Objects, cLoader.Attributes, cLoader.Values);
+            Andrews cInClose = new Andrews();
+            cInClose.Compute(cLoader.Objects, cLoader.Attributes, cLoader.Values);
+        }
+    }
+}
