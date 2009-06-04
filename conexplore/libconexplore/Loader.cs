01c7d06aa25f0dacfb5f221ebedf9ad62b3afe98
Ovidiu Sabou <ovidiu@sabou.org>
2009-06-04 11:26:53 +0300

    Save concepts to XML

---
+++ conexplore/libconexplore/Loader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace libconexplore
+{
+    public class Loader
+    {
+        public string FileName { get; private set; }
+
+        public string[] Attributes { get; private set; }
+        public string[] Objects { get; private set; }
+        public bool[][] Values { get; private set; }
+
+        public void Load(string sFileName)
+        {
+            FileName = sFileName;
+
+            List<string> lAttributes = new List<string>();
+            List<string> lObjects = new List<string>();
+            List<List<bool>> llValues = new List<List<bool>>();
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(FileName);
+
+            foreach (XmlNode xAttributeNode in xDoc.SelectNodes("/conflexplore/a"))
+            {
+                lAttributes.Add(xAttributeNode.InnerText);
+            }
+
+            foreach (XmlNode xObjectNode in xDoc.SelectNodes("/conflexplore/o"))
+            {
+                lObjects.Add(xObjectNode.InnerText);
+            }
+
+            foreach (XmlNode xRowNode in xDoc.SelectNodes("/conflexplore/r"))
+            {
+                llValues.Add(new List<bool>());
+                foreach (XmlNode xColNode in xRowNode.SelectNodes("c"))
+                {
+                    llValues[llValues.Count - 1].Add(bool.Parse(xColNode.InnerText));
+                }
+            }
+
+            Attributes = lAttributes.ToArray();
+            Objects = lObjects.ToArray();
+            
+            Values = new bool[Objects.Length][];
+            int i, j;
+            for (i = 0; i < Objects.Length; ++i)
+            {
+                Values[i] = new bool[Attributes.Length];
+                for (j = 0; j < Attributes.Length; ++j)
+                {
+                    Values[i][j] = llValues[i][j];
+                }
+            }
+        }
+
+    }
+}
---
+++ conexplore/winconexplore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using libconexplore;
+
+namespace winconexplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loader cLoader = new Loader();
+            //cLoader.Load("triangles.xml");
+            //cLoader.Load("numbers.xml");
+            cLoader.Load("mushrooms.xml");
+            //cLoader.Load("ads.xml");
+
+            //NaiveConceptProcessor cNaive = new NaiveConceptProcessor();
+            //cNaive.Process(cLoader.Objects, cLoader.Attributes, cLoader.Values);
+            
+            AndrewsConceptProcessor cAndrews = new AndrewsConceptProcessor();
+            Graph cGraph = cAndrews.Process(cLoader.Objects, cLoader.Attributes, cLoader.Values);
+
+            Console.WriteLine("Graph: {0} Concepts and {1} Edges", cGraph.Concepts.Count, cGraph.Links.Count);
+
+            //new Saver().Save("triangles-graph.xml", cGraph, cLoader.Objects, cLoader.Attributes, cLoader.Values);
+            //new Saver().Save("numbers-graph.xml", cGraph, cLoader.Objects, cLoader.Attributes, cLoader.Values);
+            new Saver().Save("mushrooms-graph.xml", cGraph, cLoader.Objects, cLoader.Attributes, cLoader.Values);
+            //new Saver().Save("ads-graph.xml", cGraph, cLoader.Objects, cLoader.Attributes, cLoader.Values);
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
