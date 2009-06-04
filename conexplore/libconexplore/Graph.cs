6bbd40c2cf73fdebae0e8707823bf86c566bc9fb
Paul V. Borza <paul@pvborza.com>
2009-06-04 11:01:50 +0300

    Saves the graph and concepts in the same file.

---
+++ conexplore/libconexplore/Graph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace libconexplore
+{
+    public class Graph
+    {
+        public List<Concept> Concepts { get; set; }
+        public List<Link> Links { get; set; }
+
+    }
+}
